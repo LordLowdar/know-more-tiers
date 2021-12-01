df848542e8e2bdc2362c5daee57b62005a40b29e
Mike Rogers <mrogers23458@gmail.com>
2021-12-01 08:58:46 -0800

    created footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// sass import
+import '../sass';
+
+//Footer component
+function Footer() {
+
+  return (
+
+    <footer></footer>
+
+  );
+  
+}
+
+export default Footer;
