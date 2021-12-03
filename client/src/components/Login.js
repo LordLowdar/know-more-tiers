0cda37258b2c7daac5f214b02b20b3c41a8fb01f
Mike Rogers <mrogers23458@gmail.com>
2021-12-03 05:41:25 -0800

    created landing, login, profile, and updated footer component

---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Footer component
+function Login() {
+
+  return (
+
+    <div className="login"></div>
+
+  );
+  
+}
+
+export default Login;
---
+++ client/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Footer component
+function Profile() {
+
+  return (
+
+    <div className="profile"></div>
+
+  );
+  
+}
+
+export default Profile;
