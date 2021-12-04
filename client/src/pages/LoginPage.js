0b0757dd4ed7ca3d1d6b86a4a7bcfb17e7a1c415
Julian Williams <julian.th.williams@gmail.com>
2021-12-04 14:27:24 -0800

    Added styling to login and register comps

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -3,20 +3,24 @@ import React from 'react';
 export default function Login() {
   return (
     <div className="base">
-      <div className="header">Login</div>
+      <div className="loginHeader">Login</div>
       <div className="content">
-        <div className="form">
+        <div className="loginForm">
           <div className="form-group">
-            <label htmlFor="username">Username</label>
+            <label className="usernameTitle" htmlFor="username">
+              Username:
+            </label>
             <input type="text" name="username" placeholder="Username" />
           </div>
           <div className="form-group">
-            <label htmlFor="password"></label>
+            <label className="passwordTitle" htmlFor="password">
+              Password:
+            </label>
             <input type="password" name="password" placeholder="Password" />
           </div>
         </div>
       </div>
-      <div className="footer">
+      <div className="loginFooter">
         <button type="button" className="btn">
           Login
         </button>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -3,24 +3,30 @@ import React from 'react';
 export default function register() {
   return (
     <div className="base">
-      <div className="header">Register</div>
+      <div className="loginHeader">Register</div>
       <div className="content">
         <div className="form">
           <div className="form-group">
-            <label htmlFor="username">Username</label>
+            <label className="usernameTitle" htmlFor="username">
+              Username:
+            </label>
             <input type="text" name="username" placeholder="Username" />
           </div>
           <div className="form-group">
-            <label htmlFor="email">Email</label>
+            <label className="usernameTitle" htmlFor="email">
+              Email:
+            </label>
             <input type="text" name="email" placeholder="email" />
           </div>
           <div className="form-group">
-            <label htmlFor="password"></label>
+            <label className="passwordTitle" htmlFor="password">
+              Password:
+            </label>
             <input type="password" name="password" placeholder="Password" />
           </div>
         </div>
       </div>
-      <div className="footer">
+      <div className="loginFooter">
         <button type="button" className="btn">
           Register
         </button>
