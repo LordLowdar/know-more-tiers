80fb61243123377d634b3681c2b8484f1f6bfa09
Julian Williams <julian.th.williams@gmail.com>
2021-12-04 11:11:28 -0800

    Merging with main in middle of work

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './sass/App.scss';
 import { Counter } from './features/counter/counter';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TierList from './components/TierList'
+import TierList from './components/TierList';
+import Login from './components/Login/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Counter />
+        <Login />
         <TierList />
         <p>
           Edit <code>src/App.js</code> and save to reload.
