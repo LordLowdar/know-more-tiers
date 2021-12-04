314f53a0d1f45a0e39fa88c945df7fcfad752ac0
Evelyn Nguyen <enguyen6208@gmail.com>
2021-12-04 10:29:33 -0800

    finished dnd

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -26,6 +26,62 @@ export const static_items = [
           content: "SubItem 21 content"
         }
       ]
-    }
+    },
+    {
+      id: '3',
+      content: "item 3 content",
+      subItems: [
+        {
+          id: '30',
+          content: "SubItem 30 content"
+        },
+        {
+          id: '31',
+          content: "SubItem 31 content"
+        }
+      ]
+    },
+    {
+      id: '4',
+      content: "item 4 content",
+      subItems: [
+        {
+          id: '40',
+          content: "SubItem 40 content"
+        },
+        {
+          id: '41',
+          content: "SubItem 41 content"
+        }
+      ]
+    },
+    {
+      id: '5',
+      content: "item 5 content",
+      subItems: [
+        {
+          id: '50',
+          content: "SubItem 50 content"
+        },
+        {
+          id: '51',
+          content: "SubItem 51 content"
+        }
+      ]
+    },
+    {
+      id: '6',
+      content: "item 6 content",
+      subItems: [
+        {
+          id: '60',
+          content: "SubItem 60 content"
+        },
+        {
+          id: '61',
+          content: "SubItem 61 content"
+        }
+      ]
+    },
   ];
   
