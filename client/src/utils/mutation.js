105c7ea55a83299a8cc3b25e7d64fa8ed80e6c77
Julian Williams <julian.th.williams@gmail.com>
2021-12-05 14:07:38 -0800

    Registration function done, still needs work on redirect and modifying profile model

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const REGISTER = gql`
+  mutation register($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        username
+      }
+    }
+  }
+`;
