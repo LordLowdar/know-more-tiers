6d18d9ce9e52c1f03509e979c10171037e2206fa
Evelyn Nguyen <enguyen6208@gmail.com>
2021-12-07 18:42:15 -0800

    finished desktop tier list styling

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -1,6 +1,7 @@
 const static_items = [
     {
       id: '1',
+      divId: 'interests-header',
       content: "Add Your Interests",
       subItems: [
         {
@@ -55,26 +56,31 @@ const static_items = [
     },
     {
       id: '2',
+      divId: "S",
       content: "S",
       subItems: []
     },
     {
       id: '3',
+      divId: "A",
       content: "A",
       subItems: []
     },
     {
       id: '4',
+      divId: "B",
       content: "B",
       subItems: []
     },
     {
       id: '5',
+      divId: "C",
       content: "C",
       subItems: []
     },
     {
       id: '6',
+      divId: "D",
       content: "D",
       subItems: []
     },
