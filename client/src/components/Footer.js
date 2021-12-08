395b15b7cd13476c6eb9826a02be482eb5ca271a
Myuze <Myuze@users.noreply.github.com>
2021-12-07 20:58:54 -0800

    fixed Footer conflict

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,15 +4,8 @@ import React from 'react';
 export default function Footer() {
 
   return (
-<<<<<<< HEAD
     <footer className="footer">
       This is a footer with content
     </footer>
-=======
-
-    <footer >
-    </footer>
-
->>>>>>> origin
   );
 }
