7175a0477eb88da7befc57ddeb55b999683f8915
Evelyn Nguyen <enguyen6208@gmail.com>
2021-12-08 17:13:09 -0800

    finished styling landing page

--- client/src/components/landing/Mission.js
+++ client/src/components/landing/Mission.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Mission() {
   return (
     <section className="mission-statement">
-      <h1>OUR MISSION STATMENT!</h1>
+      <p>
+        Finally, an app that actually connects people together based on what they have in common. Dating apps connect people together based on swiping left and right on profiles that do not have much meaning. 'KNOW MORE TIERS' matches you with like-minded people based on how you rank your interests and hobbies.
+      </p>
     </section>
   )
 }
