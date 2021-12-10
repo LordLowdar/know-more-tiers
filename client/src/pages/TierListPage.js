45bb81369acaeedda1e6c2d70199bb5f96e97c98
Myuze <Myuze@users.noreply.github.com>
2021-12-10 00:00:23 -0800

    Added Authorization usage for accessing the Tierlist page

--- client/src/pages/TierListPage.js
+++ client/src/pages/TierListPage.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
 import { NewTierList } from '../components';
 
+import { GET_ME, QUERY_USER } from '../utils/query';
+
+import Auth from '../utils/auth';
+
 export default function TierListPage() {
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery( userParam ? QUERY_USER : GET_ME, {
+    variables: { username: userParam }
+  });
+
+  const user = data?.me || data?.user || {};
+
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return (
+      <Router>
+        <>
+          <Route to="/login" />
+        </>
+      </Router>
+    )
+  }
+
+  if ( loading ) {
+    return <div> Loading... </div>;
+  }
+
+  if ( !user?.username ) {
+    return (
+      <h4>
+        You need to be logged in to create a Tierlist. Sign up or Log in!
+      </h4>
+    )
+  }
+  
   return (
     <div>
       <NewTierList />
